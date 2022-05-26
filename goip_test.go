54e09848b618f81b246d9b854490413a00841373
李光春 <gc@dtapp.net>
2022-05-27 01:15:24 +0800

    - update

--- goip_test.go
+++ goip_test.go
@@ -9,24 +9,56 @@ func lod() *Client {
 	return NewIp()
 }
 
-func TestIp(t *testing.T) {
+func TestTo4(t *testing.T) {
+	t.Logf(string(net.ParseIP("61.241.55.180").To4()))
+}
+
+func BenchmarkTo4(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b.Logf(string(net.ParseIP("61.241.55.180").To4()))
+	}
+}
+
+func TestTo16(t *testing.T) {
+	t.Logf(string(net.ParseIP("240e:3b4:38e4:3295:7093:af6c:e545:f2e9").To16()))
+}
+
+func BenchmarkTo16(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b.Logf(string(net.ParseIP("240e:3b4:38e4:3295:7093:af6c:e545:f2e9").To16()))
+	}
+}
+
+func TestIpv4(t *testing.T) {
 	client := lod()
-	t.Logf("%+v", net.ParseIP("61.241.55.180").To4())
-	t.Logf("%+v", net.ParseIP("240e:3b4:38e4:3295:7093:af6c:e545:f2e9").To16())
-	t.Logf("%+v", client.V4db.Find("61.241.55.180"))
-	t.Logf("%+v", client.V4db.Find("116.7.98.130"))
-	t.Logf("%+v", client.V6db.Find("240e:3b4:38e4:3295:7093:af6c:e545:f2e9"))
-	t.Log(client.V4Region.MemorySearch("61.241.55.180"))
+	t.Log(client.V4db.Find("116.7.98.130"))
 }
 
 func BenchmarkIpv4(b *testing.B) {
 	client := lod()
 	for i := 0; i < b.N; i++ {
-		b.Log(client.V4db.Find("61.241.55.180"))
+		b.Log(client.V4db.Find("116.7.98.130"))
 	}
 }
 
-func BenchmarkIpv5(b *testing.B) {
+func TestV4Region(t *testing.T) {
+	client := lod()
+	t.Log(client.V4Region.MemorySearch("61.241.55.180"))
+}
+
+func BenchmarkV4Region(b *testing.B) {
+	client := lod()
+	for i := 0; i < b.N; i++ {
+		b.Log(client.V4Region.MemorySearch("61.241.55.180"))
+	}
+}
+
+func TestIpv6(t *testing.T) {
+	client := lod()
+	t.Log(client.V6db.Find("116.7.98.130"))
+}
+
+func BenchmarkIpv6(b *testing.B) {
 	client := lod()
 	for i := 0; i < b.N; i++ {
 		b.Log(client.V6db.Find("240e:3b4:38e4:3295:7093:af6c:e545:f2e9"))
